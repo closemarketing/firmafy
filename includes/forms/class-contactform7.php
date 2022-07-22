dfc4020a72683a2804c7f85890a4676101a407ae
David Perez <david@closemarketing.es>
2022-07-22 13:51:49 +0200

    finished contact forms

--- includes/forms/class-contactform7.php
+++ includes/forms/class-contactform7.php
@@ -21,19 +21,12 @@ defined( 'ABSPATH' ) || exit;
 class Firmafy_CF7_Settings {
 
 	/**
-	 * CRM LIB external
-	 *
-	 * @var obj
-	 */
-	private $crmlib;
-
-	/**
 	 * Construct of class
 	 */
 	public function __construct() {
 		add_filter( 'wpcf7_editor_panels', array( $this, 'show_cm_metabox' ) );
-		add_action( 'wpcf7_after_save', array( $this, 'crm_save_options' ) );
-		add_action( 'wpcf7_before_send_mail', array( $this, 'crm_process_entry' ) );
+		add_action( 'wpcf7_after_save', array( $this, 'firmafy_save_options' ) );
+		add_action( 'wpcf7_before_send_mail', array( $this, 'firmafy_process_entry' ) );
 	}
 
 	/**
@@ -60,29 +53,28 @@ class Firmafy_CF7_Settings {
 	 * @return void
 	 */
 	public function settings_add_firmafy( $args ) {
+		global $helpers_firmafy;
 
-		$cf7_crm_defaults = array();
-		$cf7_crm          = get_option( 'cf7_crm_' . $args->id(), $cf7_crm_defaults );
+		$cf7_firmafy_defaults = array();
+		$cf7_firmafy          = get_option( 'cf7_firmafy_' . $args->id(), $cf7_firmafy_defaults );
 		?>
 		<div class="metabox-holder">
 			<div class="cme-main-fields">
 				<p>
-					<label for="wpcf7-crm-fc_crm_username"><?php esc_html_e( 'URL:', 'formscrm' ); ?></label><br />
-					<input type="text" id="wpcf7-crm-fc_crm_username" name="wpcf7-crm[fc_crm_username]" class="wide" size="70" placeholder="[<?php esc_html_e( 'CRM Username', 'formscrm' ); ?>]" value="<?php echo ( isset( $cf7_crm['fc_crm_username'] ) ) ? esc_attr( $cf7_crm['fc_crm_username'] ) : ''; ?>" />
-				</p>
-				<p>
-					<label for="wpcf7-crm-fc_crm_password"><?php esc_html_e( 'Password:', 'formscrm' ); ?></label><br />
-					<input type="password" id="wpcf7-crm-fc_crm_password" name="wpcf7-crm[fc_crm_password]" class="wide" size="70" placeholder="[<?php esc_html_e( 'CRM Password', 'formscrm' ); ?>]" value="<?php echo ( isset( $cf7_crm['fc_crm_password'] ) ) ? esc_attr( $cf7_crm['fc_crm_password'] ) : ''; ?>" />
-				</p>
-				<p>
-					<select name="wpcf7-crm[fc_crm_module]" class="medium" onchange="jQuery(this).parents('form').submit();" id="fc_crm_module">
+					<label for="wpcf7-firmafy-firmafy_template"><?php esc_html_e( 'Template:', 'firmafy' ); ?></label><br />
+					<select name="wpcf7-firmafy[firmafy_template]" class="medium" onchange="jQuery(this).parents('form').submit();" id="firmafy_template">
 						<?php
-						foreach ( $this->crmlib->list_modules( $cf7_crm ) as $module ) {
-							echo '<option value="' . esc_html( $module['name'] ) . '" ';
-							if ( isset( $module['name'] ) ) {
-								selected( $cf7_crm['fc_crm_module'], $module['name'] );
+						echo '<option value="" ';
+						if ( empty( $template['value'] ) ) {
+							selected( $cf7_firmafy['firmafy_template'], '' );
+						}
+						echo '>' . __( 'Select template', 'firmafy' ) . '</option>';
+						foreach ( $helpers_firmafy->get_templates() as $template ) {
+							echo '<option value="' . esc_html( $template['value'] ) . '" ';
+							if ( isset( $template['value'] ) ) {
+								selected( $cf7_firmafy['firmafy_template'], $template['value'] );
 							}
-							echo '>' . esc_html( $module['label'] ) . '</option>';
+							echo '>' . esc_html( $template['label'] ) . '</option>';
 						}
 						?>
 					</select>
@@ -90,24 +82,24 @@ class Firmafy_CF7_Settings {
 			</div>
 
 		<?php
-		if ( isset( $cf7_crm['fc_crm_module'] ) && $cf7_crm['fc_crm_module'] ) {
-			$crm_fields = $this->crmlib->list_fields( $cf7_crm );
+		if ( isset( $cf7_firmafy['firmafy_template'] ) && $cf7_firmafy['firmafy_template'] ) {
+			$firmafy_fields = $helpers_firmafy->get_variables_template( $cf7_firmafy['firmafy_template'] );
 			?>
 			<table class="cf7-map-table" cellspacing="0" cellpadding="0">
 				<tbody>
 					<tr class="cf7-map-row">
-						<th class="cf7-map-column cf7-map-column-heading cf7-map-column-key"><?php esc_html_e( 'Field CRM', 'formscrm' ); ?></th>
-						<th class="cf7-map-column cf7-map-column-heading cf7-map-column-value"><?php esc_html_e( 'Form Field', 'formscrm' ); ?></th>
+						<th class="cf7-map-column cf7-map-column-heading cf7-map-column-key"><?php esc_html_e( 'Field Template', 'firmafy' ); ?></th>
+						<th class="cf7-map-column cf7-map-column-heading cf7-map-column-value"><?php esc_html_e( 'Form Field', 'firmafy' ); ?></th>
 					</tr>
 						<?php
-						foreach ( $crm_fields as $crm_field ) {
+						foreach ( $firmafy_fields as $firmafy_field ) {
 							?>
 							<tr class="cf7-map-row">
 									<td class="cf7-map-column cf7-map-column-key">
-										<label for="wpcf7-crm-field-<?php echo esc_html( $crm_field['name'] ); ?>"><?php echo esc_html( $crm_field['label'] ); ?><?php if ( $crm_field['required'] ) { echo ' <span class="required">*</span>'; } ?></label>
+										<label for="wpcf7-firmafy-field-<?php echo esc_html( $firmafy_field['name'] ); ?>"><?php echo esc_html( $firmafy_field['label'] ); ?><?php if ( $firmafy_field['required'] ) { echo ' <span class="required">*</span>'; } ?></label>
 									</td>
 									<td class="cf7-map-column cf7-map-column-value">
-										<input type="text" id="wpcf7-crm-field-<?php echo esc_html( $crm_field['name'] ); ?>" name="wpcf7-crm[fc_crm_field-<?php echo esc_html( $crm_field['name'] ); ?>]" class="wide" size="70" placeholder="[<?php esc_html_e( 'Name of your field', 'formscrm' ); ?>]" value="<?php echo ( isset( $cf7_crm[ 'fc_crm_field-' . $crm_field['name'] ] ) ) ? esc_attr( $cf7_crm[ 'fc_crm_field-' . $crm_field['name'] ] ) : ''; ?>" <?php if ( $crm_field['required'] ) { echo ' required'; } ?>/>
+										<input type="text" id="wpcf7-firmafy-field-<?php echo esc_html( $firmafy_field['name'] ); ?>" name="wpcf7-firmafy[firmafy_field-<?php echo esc_html( $firmafy_field['name'] ); ?>]" class="wide" size="70" placeholder="[<?php esc_html_e( 'Name of your field', 'firmafy' ); ?>]" value="<?php echo ( isset( $cf7_firmafy[ 'firmafy_field-' . $firmafy_field['name'] ] ) ) ? esc_attr( $cf7_firmafy[ 'firmafy_field-' . $firmafy_field['name'] ] ) : ''; ?>" <?php if ( $firmafy_field['required'] ) { echo ' required'; } ?>/>
 									</td>
 							</tr>
 						<?php } ?>
@@ -124,10 +116,15 @@ class Firmafy_CF7_Settings {
 	 * @param obj $args Arguments CF7.
 	 * @return void
 	 */
-	public function crm_save_options( $args ) {
+	public function firmafy_save_options( $args ) {
 
-		if ( isset( $_POST['wpcf7-crm'] ) && is_array( $_POST['wpcf7-crm'] ) ) {
-			update_option( 'cf7_crm_' . $args->id, array_filter( $_POST['wpcf7-crm'] ) );
+		if ( isset( $_POST['wpcf7-firmafy'] ) && is_array( $_POST['wpcf7-firmafy'] ) ) {
+			$settings_firmafy     = array_filter( $_POST['wpcf7-firmafy'] );
+			$settings_firmafy_san = array();
+			foreach ( $settings_firmafy as $key => $value ) {
+				$settings_firmafy_san[ $key ] = sanitize_text_field( $value );
+			}
+			update_option( 'cf7_firmafy_' . $args->id, $settings_firmafy_san );
 		}
 	}
 
@@ -137,25 +134,18 @@ class Firmafy_CF7_Settings {
 	 * @param obj $obj CF7 Object.
 	 * @return void
 	 */
-	public function crm_process_entry( $obj ) {
-
-		$cf7_crm    = get_option( 'cf7_crm_' . $obj->id() );
+	public function firmafy_process_entry( $obj ) {
+		global $helpers_firmafy;
+		$cf7_firmafy    = get_option( 'cf7_firmafy_' . $obj->id() );
 		$submission = WPCF7_Submission::get_instance();
 
-		if ( $cf7_crm ) {
-			$merge_vars = $this->get_merge_vars( $cf7_crm, $submission->get_posted_data() );
+		if ( $cf7_firmafy ) {
+			$merge_vars = $this->get_merge_vars( $cf7_firmafy, $submission->get_posted_data() );
 
-			if ( isset( $_COOKIE['vk'] ) ) {
-				$merge_vars[] = array(
-					'name'  => 'visitor_key',
-					'value' => esc_attr( $_COOKIE['vk'] ),
-				);
-			}
-
-			$response_result = $this->crmlib->create_entry( $cf7_crm, $merge_vars );
+			$response_result = $helpers_firmafy->create_entry( $cf7_firmafy['firmafy_template'], $merge_vars );
 
 			if ( 'error' === $response_result['status'] ) {
-				formscrm_debug_email_lead( $cf7_crm['fc_crm_type'], 'Error ' . $response_result['message'], $merge_vars );
+				firmafy_debug_email_lead( $cf7_firmafy['fc_firmafy_type'], 'Error ' . $response_result['message'], $merge_vars );
 			} else {
 				error_log( $response_result['id'] );
 			}
@@ -165,17 +155,17 @@ class Firmafy_CF7_Settings {
 	/**
 	 * Extract merge variables
 	 *
-	 * @param array $cf7_crm Array settings from CRM.
+	 * @param array $cf7_firmafy Array settings from CRM.
 	 * @param array $submitted_data Submitted data.
 	 * @return array
 	 */
-	private function get_merge_vars( $cf7_crm, $submitted_data ) {
+	private function get_merge_vars( $cf7_firmafy, $submitted_data ) {
 		$merge_vars = array();
-		foreach ( $cf7_crm as $key => $value ) {
-			if ( false !== strpos( $key, 'fc_crm_field' ) ) {
-				$crm_key      = str_replace( 'fc_crm_field-', '', $key );
+		foreach ( $cf7_firmafy as $key => $value ) {
+			if ( false !== strpos( $key, 'firmafy_field' ) ) {
+				$firmafy_key      = str_replace( 'firmafy_field-', '', $key );
 				$merge_vars[] = array(
-					'name'  => $crm_key,
+					'name'  => $firmafy_key,
 					'value' => isset( $submitted_data[ $value ] ) ? $submitted_data[ $value ] : '',
 				);
 			}
