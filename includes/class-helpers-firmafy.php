59f2421ba092d6098e92016b4f5d38e6b3b32928
David Perez <david@closemarketing.es>
2022-11-15 08:38:10 +0100

    remove html not compatible with html2pdf

--- includes/class-helpers-firmafy.php
+++ includes/class-helpers-firmafy.php
@@ -376,6 +376,9 @@ class Helpers_Firmafy {
 		$content = str_replace( '<figure', '<div', $content );
 		$content = str_replace( '</figure', '</div', $content );
 
+		$content = str_replace( '<section', '<div', $content );
+		$content = str_replace( '</section', '</div', $content );
+
 		// Replace date.
 		$content = str_replace( '{fecha}', date( 'd-m-Y' ), $content );
 
