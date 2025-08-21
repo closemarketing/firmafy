8f0b5419ce71b8947a3ef8b5700933c7a280573f
davidperezgar <david@closemarketing.es>
2025-08-21 09:01:43 +0200

    make tests

---
+++ tests/Unit/test-products-sign-order.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class ProductsOrderSignTest
+ *
+ * @package Firmafy
+ */
+
+/**
+ * Sample test case.
+ */
+class ProductsOrderSignTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
